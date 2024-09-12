12e4f23a7c458c4e532e6674729935d0a9f7f1a3
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2024-09-12 22:13:06 +0300

    basic setup and basic unit tests

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+
+
+def main():
+    tn = TextNode('Text node', 'Italic', 'https://www.boot.dev')
+    print(tn)
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_eq_with_url(self):
+        node = TextNode("This is a text node", "bold", "boot.dev")
+        node2 = TextNode("This is a text node", "bold", "boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is not a text node", "bold")
+        self.assertNotEquals(node, node2)
+
+    def test_not_eq_with_url(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold", "boot.dev")
+        self.assertNotEquals(node, node2)
+
+    def test_not_eq_text_type(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "italic")
+        self.assertNotEquals(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
